f0feb6685596d0021185c533a3331587684f767c
Zeke Zhang <sean1832725142@gmail.com>
2024-01-10 23:24:49 +1100

    Update build.bat script to include a batch file for running the built UDP chat application

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 if not exist dist (
-  mkdir dist
+    mkdir dist
 )
 copy main.py dist\udp-chat.py
-echo Build completed at dist\udp-chat.py.
+echo @python "%~dp0dist\udp-chat.py" %*>dist\udp-chat.bat
+echo Build completed at dist\udp-chat.bat.
