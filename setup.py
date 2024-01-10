9eacc89a6fcb3a5d9ba87fa9429b92b25cd351c1
Zeke Zhang <sean1832725142@gmail.com>
2024-01-11 01:34:42 +1100

    chore: bump version to `0.0.2` for setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='udp-chat',
-    version='0.0.1',
+    version='0.0.2',
     author='Zeke Zhang',
     description='cli chat application using udp',
     scripts=['main.py'],
