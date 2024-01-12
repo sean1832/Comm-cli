a59e5ebb5d0b14d4af646c89fae4dca2ffe78b62
Zeke Zhang <sean1832725142@gmail.com>
2024-01-13 03:02:44 +1100

    chore: remove installation script

--- install.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pip install -e .
-
-echo \n\n Installation complete. \n\n Please run udp-chat -h for help.
--- install.sh
+++
@@ -1,4 +0,0 @@
-pip install -e .
-echo 
-echo Installation complete. 
-echo Please run udp-chat -h for help.
