455577501a781e66bff0ad066d074f87c7801515
Zeke Zhang <sean1832725142@gmail.com>
2024-01-13 19:43:30 +1100

    build: modify to become `nx` package

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='nx',
     version='0.0.8',
+    package_dir={'': 'nx'},
+    packages=find_packages(where='nx'),
     author='Zeke Zhang',
     description='Simple local network data transfer CLI. Supports file transfer (udp and tcp) and text transfer.',
-    scripts=['main.py', 'progress_bar.py', 'utilities.py'],
     entry_points={
         'console_scripts': [
-            'nx = main:main',
+            'nx = nx.main:main',
         ],
     },
 )
